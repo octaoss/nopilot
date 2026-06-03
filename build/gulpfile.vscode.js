fc323a3cc59f08d6894eb7537b3822fe46bf5323
Yohanes Oktanio <yohanesoktayt72@gmail.com>
2026-06-04 04:36:24 +0700

    update: Applying a fix to the Windows build failure by patching the Gulpfile and updating the GitHub Actions workflow for comprehensive Linux packaging.

--- build/gulpfile.vscode.js
+++ build/gulpfile.vscode.js
@@ -447,7 +447,17 @@ function patchWin32DependenciesTask(destinationFolderName) {
 	const cwd = path.join(path.dirname(root), destinationFolderName);
 
 	return async () => {
-		const deps = await glob('**/*.node', { cwd, ignore: 'extensions/node_modules/@parcel/watcher/**' });
+		const deps = await glob('**/*.node', {
+			cwd,
+			ignore: [
+				'extensions/node_modules/@parcel/watcher/**',
+				'**/node_modules/**/prebuilds/linux-x64/**',
+				'**/node_modules/**/prebuilds/linux-arm64/**',
+				'**/node_modules/**/prebuilds/linux-armhf/**',
+				'**/node_modules/**/prebuilds/darwin-x64/**',
+				'**/node_modules/**/prebuilds/darwin-arm64/**'
+			]
+		});
 		const packageJson = JSON.parse(await fs.promises.readFile(path.join(cwd, 'resources', 'app', 'package.json'), 'utf8'));
 		const product = JSON.parse(await fs.promises.readFile(path.join(cwd, 'resources', 'app', 'product.json'), 'utf8'));
 		const baseVersion = packageJson.version.replace(/-.*$/, '');
