81c45c4daf0afa3a0c20ccb8f618b010d061d1fe
Yohanes Oktanio <yohanesoktayt72@gmail.com>
2026-06-05 00:50:17 -0700

    Fix/windows (#17)
    * fix: patch blackscreen again windows build
    
    * add: self host runner;
    
    * fix naming self host runner
    
    * fix: build dependencies linux deb
    
    * fix: win2AppUserModelID
    
    * fix: runner add autorun
    
    * fix: buildwindows|2

--- build/gulpfile.vscode.js
+++ build/gulpfile.vscode.js
@@ -142,11 +142,11 @@ const bundleVSCodeTask = task.define('bundle-vscode', task.series(
 				],
 				resources: vscodeResources,
 				fileContentMapper: filePath => {
+					const normalizedPath = filePath.replace(/\\/g, '/').toLowerCase();
 					if (
-						filePath.endsWith('vs/code/electron-sandbox/workbench/workbench.js') ||
-						// TODO: @justchen https://github.com/microsoft/vscode/issues/213332 make sure to remove when we use window.open on desktop
-						filePath.endsWith('vs/workbench/contrib/issue/electron-sandbox/issueReporter.js') ||
-						filePath.endsWith('vs/code/electron-sandbox/processExplorer/processExplorer.js')) {
+						normalizedPath.endsWith('vs/code/electron-sandbox/workbench/workbench.js') ||
+						normalizedPath.endsWith('vs/workbench/contrib/issue/electron-sandbox/issueReporter.js') ||
+						normalizedPath.endsWith('vs/code/electron-sandbox/processExplorer/processExplorer.js')) {
 						return async (content) => {
 							const bootstrapWindowContent = await fs.promises.readFile(path.join(root, 'out-build', 'bootstrap-window.js'), 'utf-8');
 							return `${bootstrapWindowContent}\n${content}`; // prepend bootstrap-window.js content to entry points that are Electron windows
@@ -154,11 +154,13 @@ const bundleVSCodeTask = task.define('bundle-vscode', task.series(
 					}
 					return undefined;
 				},
-				skipTSBoilerplateRemoval: entryPoint =>
-					entryPoint === 'vs/code/electron-sandbox/workbench/workbench' ||
-					// TODO: @justchen https://github.com/microsoft/vscode/issues/213332 make sure to remove when we use window.open on desktop
-					entryPoint === 'vs/workbench/contrib/issue/electron-sandbox/issueReporter' ||
-					entryPoint === 'vs/code/electron-sandbox/processExplorer/processExplorer',
+				skipTSBoilerplateRemoval: entryPoint => {
+					const normalizedEntryPoint = entryPoint.replace(/\\/g, '/').toLowerCase();
+					return normalizedEntryPoint === 'vs/code/electron-sandbox/workbench/workbench' ||
+						// TODO: @justchen https://github.com/microsoft/vscode/issues/213332 make sure to remove when we use window.open on desktop
+						normalizedEntryPoint === 'vs/workbench/contrib/issue/electron-sandbox/issueReporter' ||
+						normalizedEntryPoint === 'vs/code/electron-sandbox/processExplorer/processExplorer';
+				},
 			}
 		}
 	)
@@ -454,27 +456,31 @@ function patchWin32DependenciesTask(destinationFolderName) {
 
 		// Filter out non-Windows native modules and parcel watcher
 		deps = deps.filter(dep => {
-			const isParcelWatcher = dep.includes('extensions/node_modules/@parcel/watcher');
-			const isNonWindowsPrebuild = dep.includes('prebuilds/linux-') || dep.includes('prebuilds/darwin-');
+			const normalizedDep = dep.replace(/\\/g, '/').toLowerCase();
+			const isParcelWatcher = normalizedDep.includes('node_modules/@parcel/watcher');
+			const isNonWindowsPrebuild = normalizedDep.includes('/prebuilds/linux') || normalizedDep.includes('/prebuilds/darwin') || normalizedDep.includes('musl.node') || normalizedDep.includes('-linux-') || normalizedDep.includes('-darwin-');
 			return !isParcelWatcher && !isNonWindowsPrebuild;
 		});
 
 		await Promise.all(deps.map(async dep => {
 			const basename = path.basename(dep);
-
-			await rcedit(path.join(cwd, dep), {
-				'file-version': baseVersion,
-				'version-string': {
-					'CompanyName': 'Microsoft Corporation',
-					'FileDescription': product.nameLong,
-					'FileVersion': packageJson.version,
-					'InternalName': basename,
-					'LegalCopyright': 'Copyright (C) 2022 Microsoft. All rights reserved',
-					'OriginalFilename': basename,
-					'ProductName': product.nameLong,
-					'ProductVersion': packageJson.version,
-				}
-			});
+			try {
+				await rcedit(path.join(cwd, dep), {
+					'file-version': baseVersion,
+					'version-string': {
+						'CompanyName': 'Microsoft Corporation',
+						'FileDescription': product.nameLong,
+						'FileVersion': packageJson.version,
+						'InternalName': basename,
+						'LegalCopyright': 'Copyright (C) 2024 Microsoft. All rights reserved',
+						'OriginalFilename': basename,
+						'ProductName': product.nameLong,
+						'ProductVersion': packageJson.version,
+					}
+				});
+			} catch (e) {
+				console.warn(`Could not patch ${dep}: ${e.message}`);
+			}
 		}));
 	};
 }
